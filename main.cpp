d15484ed16196d698dabd53d1a05203d6650deff
vmyakovlev <vovchkir@gmail.com>
2018-04-20 02:49:35 +0400

    add generate blocks logic

--- main.cpp
+++ main.cpp
@@ -21,38 +21,67 @@
 
 
 std::vector<std::thread> threadA;
-static std::mutex thrA_mut;
+std::vector<std::thread> threadB;
+std::mutex mutex_add;
+
 
 //template <typename Type>
 
+static std::list<std::vector<uint8_t>> blocks;
+static std::list<std::vector<uint8_t>> blocks;
 
-void Generate( size_t block_count, size_t size)
+
+void Generate(size_t id,  size_t block_count, size_t size)
 {
     std::random_device rd;
-    std::mt19937 gen(rd());
+    std::minstd_rand gen(rd());
+    while (blocks.size()<block_count) {
+        if (!mutex_add.try_lock())
+            continue;
+        blocks.push_back(std::vector<uint8_t>(size));
+        mutex_add.unlock();
 
-    std::list<std::vector<uint8_t>> blocks;
+        //auto& data = blocks.back();//end();
 
-    blocks.resize(block_count);
-    for(auto& block:blocks)
-    {
-        block.resize(size);
-        for(auto& elem:block)
+        for(auto& elem: blocks.back())
             elem = gen();
+
+        std::this_thread::yield();
     }
-
-    std::lock_guard<std::mutex> guard(thrA_mut);
-    std::cout << "Block[" << block_count << "][" << size << "]:" << std::endl;
-    for(const auto block:blocks)
-        for(const auto elem:block)
-            std::cout << std::showbase << std::hex << std::setw(2) << static_cast<size_t>(elem) << " ";
-    std::cout << std::endl;
-
 }
 
-void CalcCRC()
+void CalcCRC(size_t id,  size_t block_count, size_t size)
 {
+static const POLY(0x82f63b78);
 
+/* CRC-32 (Ethernet, ZIP, etc.) polynomial in reversed bit order. */
+/* #define POLY 0xedb88320 */
+    std::vector<uint8_t> buff;
+    size_t crc32(0);
+    for(const auto& elem:buff)
+    {
+        crc32 ^= elem;
+        for (k = 0; k < 8; k++)
+            crc32 = crc32 & 1 ? (crc32 >> 1) ^ POLY : crc32 >> 1;
+    }
+//while (len--) {
+//        crc ^= *buf++;
+//        for (k = 0; k < 8; k++)
+//            crc = crc & 1 ? (crc >> 1) ^ POLY : crc >> 1;
+//    }
+
+//uint32_t crc32c(uint32_t crc, const unsigned char *buf, size_t len)
+//{
+//    int k;
+
+//    crc = ~crc;
+//    while (len--) {
+//        crc ^= *buf++;
+//        for (k = 0; k < 8; k++)
+//            crc = crc & 1 ? (crc >> 1) ^ POLY : crc >> 1;
+//    }
+//    return ~crc;
+//}
 }
 
 int main(int argc, char **argv){
@@ -60,30 +89,49 @@ int main(int argc, char **argv){
 
     ConfigParser config(argc, argv);
 
-    size_t threadA_count = config.GetThreadAcount();
-    size_t threadB_count = config.GetThreadBcount();
-    size_t block_count = config.GetBlockCount();
-    size_t block_size = config.GetBlockSize();
-    size_t unknown_int = config.getNumericValueOption("-t");
-    std::string unknown_str = config.getSringValueOption("-t");
-    size_t unknown_int1 = config.getNumericValueOption("-a");
-    std::string unknown_str1 = config.getSringValueOption("-a");
-    bool exist1 = config.OptionExists("-t");
-    bool exist2 = config.OptionExists("-a");
-
-    threadA.resize(threadA_count);
+    threadA.resize(config.GetThreadAcount());
+    threadB.resize(config.GetThreadBcount());
 
     for (auto& thr:threadA)
     {
         static size_t idx(0);
-        thr = std::thread(&Generate, block_count, block_size);
+        thr = std::thread(&Generate, ++idx, config.GetBlockCount(), config.GetBlockSize());
     }
+    for (auto& thr:threadB)
+    {
+        static size_t idx(0);
+        thr = std::thread(&CalcCRC, ++idx, config.GetBlockCount(), config.GetBlockSize());
+    }
+
 
 
     for (auto& thr:threadA)
         if(thr.joinable())
             thr.join();
 
+    for (auto& thr:threadB)
+        if(thr.joinable())
+            thr.join();
 
+    for(const auto block:blocks)
+    {
+        static size_t idx(0);
+        std::cout << std::dec <<"Block[" << ++idx << "][" << config.GetBlockSize() << "]:" << std::endl;
+        for(const auto elem:block)
+            std::cout << std::showbase << std::hex << std::setw(2) << static_cast<size_t>(elem) << " ";
+        std::cout << std::endl;
+    }
     return 0;
 }
+
+
+//    size_t threadA_count = config.GetThreadAcount();
+//    size_t threadB_count = config.GetThreadBcount();
+//    size_t block_count = config.GetBlockCount();
+//    size_t block_size = config.GetBlockSize();
+//    size_t unknown_int = config.getNumericValueOption("-t");
+//    std::string unknown_str = config.getSringValueOption("-t");
+//    size_t unknown_int1 = config.getNumericValueOption("-a");
+//    std::string unknown_str1 = config.getSringValueOption("-a");
+//    bool exist1 = config.OptionExists("-t");
+//    bool exist2 = config.OptionExists("-a");
